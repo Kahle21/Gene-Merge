ea96ee171a124de1538f24bb3c5702736835199b
Kahle <salisburykahle@gmail.com>
2025-11-04 23:49:48 -0600

    chore: initial Unity project with GameEntry bootstrap

---
+++ Assets/Scripts/Core/GameEntry.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameEntry : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Gene Merge starting up...");
+        // Load main menu or test scene later
+    }
+}
+
